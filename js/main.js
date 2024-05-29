56a41cb0abfdce096c96fc2c48a51ab1a0d58f04
annaritadituri <annaritadituri@icloud.com>
2024-05-29 18:20:36 +0200

    Email list from API

--- js/main.js
+++ js/main.js
@@ -2,12 +2,38 @@
 
 //////////////////////////////////////////////////////////
 
-const { createApp } = Vue
+  const { createApp } = Vue
 
   createApp({
     data() {
       return {
-        message: 'Hello Vue!'
+
+        url: "https://flynn.boolean.careers/exercises/api/random/mail",
+        emails: [],
+        numEmails: 10,
+
+      }
+    },
+
+    methods: {
+
+      apiCall() {
+
+        axios.get(this.url).then((response) => {
+          this.emails.push(response.data.response);
+        })
+
+      },
+
+      isLoading () {
+        if (this.emails.length < this.numEmails) return true;
+      },
+
+    },
+
+    created() {
+      for(let i = 0; i < this.numEmails; i++) {
+        this.apiCall();
       }
     }
   }).mount('#app')
